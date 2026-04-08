b386eee0a97163b4a04d6ced9ae260a20d51e32c
NicholasDewar <268340690+NicholasDewar@users.noreply.github.com>
2026-04-08 10:21:59 +0800

    Update main.go

--- go/installer/main.go
+++ go/installer/main.go
@@ -22,7 +22,7 @@ import (
 )
 
 const (
-	version     = "v0.4.9"
+	version     = "v0.5.3"
 	repoOwner   = "NicholasDewar"
 	repoName    = "Wuthering_Waves_Private_Server"
 	installDir  = "/etc/wwps/tgbot"
