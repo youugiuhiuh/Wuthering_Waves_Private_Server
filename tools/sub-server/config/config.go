0f2638defeeedbc8110c24db12cc67227f17a231
youugiuhiuh <260548057+youugiuhiuh@users.noreply.github.com>
2026-07-07 20:01:46 +0800

    fix(sub): include TLS cert/key in systemd unit, load config.json in sub-server

--- tools/sub-server/config/config.go
+++ tools/sub-server/config/config.go
@@ -1,24 +1,84 @@
 package config
 
-import "flag"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const defaultConfigPath = "/etc/wwps/sub-server/config.json"
 
 type Config struct {
-	ListenAddr string
-	TLSCert    string
-	TLSKey     string
-	AegisGrpc  string
-	RateLimit  int
-	CacheTTL   int
+	ListenAddr string `json:"listen_addr"`
+	TLSCert    string `json:"tls_cert"`
+	TLSKey     string `json:"tls_key"`
+	AegisGrpc  string `json:"aegis_grpc"`
+	RateLimit  int    `json:"rate_limit"`
+	CacheTTL   int    `json:"cache_ttl"`
+}
+
+func defaultConfig() *Config {
+	exe, _ := os.Executable()
+	cfgDir := filepath.Dir(exe)
+	defaultCert := cfgDir + "/certs/fullchain.pem"
+	defaultKey := cfgDir + "/certs/privkey.pem"
+	return &Config{
+		ListenAddr: ":8443",
+		TLSCert:    defaultCert,
+		TLSKey:     defaultKey,
+		AegisGrpc:  "unix:///var/run/aegis/sub.sock",
+		RateLimit:  10,
+		CacheTTL:   60,
+	}
+}
+
+func loadConfigFile(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	cfg := &Config{}
+	if err := json.Unmarshal(data, cfg); err != nil {
+		return nil, fmt.Errorf("parse config file: %w", err)
+	}
+	return cfg, nil
 }
 
 func Parse() *Config {
-	cfg := &Config{}
-	flag.StringVar(&cfg.ListenAddr, "listen-addr", ":8443", "listen address")
-	flag.StringVar(&cfg.TLSCert, "tls-cert", "", "TLS certificate path")
-	flag.StringVar(&cfg.TLSKey, "tls-key", "", "TLS key path")
-	flag.StringVar(&cfg.AegisGrpc, "aegis-grpc", "unix:///var/run/aegis/sub.sock", "Aegis gRPC socket")
-	flag.IntVar(&cfg.RateLimit, "rate-limit", 10, "requests per minute per token")
-	flag.IntVar(&cfg.CacheTTL, "cache-ttl", 60, "cache TTL in seconds")
+	cfg := defaultConfig()
+
+	// Try loading from config file first
+	if fileCfg, err := loadConfigFile(defaultConfigPath); err == nil {
+		if fileCfg.ListenAddr != "" {
+			cfg.ListenAddr = fileCfg.ListenAddr
+		}
+		if fileCfg.TLSCert != "" {
+			cfg.TLSCert = fileCfg.TLSCert
+		}
+		if fileCfg.TLSKey != "" {
+			cfg.TLSKey = fileCfg.TLSKey
+		}
+		if fileCfg.AegisGrpc != "" {
+			cfg.AegisGrpc = fileCfg.AegisGrpc
+		}
+		if fileCfg.RateLimit > 0 {
+			cfg.RateLimit = fileCfg.RateLimit
+		}
+		if fileCfg.CacheTTL > 0 {
+			cfg.CacheTTL = fileCfg.CacheTTL
+		}
+	}
+
+	// CLI flags override config file
+	flag.StringVar(&cfg.ListenAddr, "listen-addr", cfg.ListenAddr, "listen address")
+	flag.StringVar(&cfg.TLSCert, "tls-cert", cfg.TLSCert, "TLS certificate path")
+	flag.StringVar(&cfg.TLSKey, "tls-key", cfg.TLSKey, "TLS key path")
+	flag.StringVar(&cfg.AegisGrpc, "aegis-grpc", cfg.AegisGrpc, "Aegis gRPC socket")
+	flag.IntVar(&cfg.RateLimit, "rate-limit", cfg.RateLimit, "requests per minute per token")
+	flag.IntVar(&cfg.CacheTTL, "cache-ttl", cfg.CacheTTL, "cache TTL in seconds")
 	flag.Parse()
+
 	return cfg
 }
